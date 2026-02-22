fd255893ba56898ece9b75f1dd2205d5bb4d56a4
kien-ship-it <ldkientzs@gmail.com>
2026-02-22 04:44:25 -0500

    setting up env and venv

---
+++ test.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+
+load_dotenv()
+
+endpoint = os.getenv("endpoint")
+model_name = os.getenv("model_name")
+deployment = os.getenv("deployment")
+subscription_key = os.getenv("subscription_key")
+api_version = os.getenv("api_version")
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "I am going to Paris, what should I see?",
+        }
+    ],
+    max_completion_tokens=16384,
+    model=deployment
+)
+
+print(response.choices[0].message.content)
